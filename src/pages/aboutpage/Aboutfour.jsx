cdc974c288ebb6da8e83190613af20037ba9c204
unknown <daveorjih943@gmail.com>
2026-05-29 10:53:48 +0100

    my-resturant

--- src/pages/aboutpage/Aboutfour.jsx
+++ src/pages/aboutpage/Aboutfour.jsx
@@ -10,7 +10,7 @@ function Aboutfour() {
             <Goldtext Text="What they say"/>
             <p>Lorem ipsum dolor sit amet consectetur. Aliquet leo nec integer enim. Eu vulputate in turpis magna vulputate sed ultricies vel. Lorem pellentesque mi quam ut. Convallis cursus augue nulla vulputate tincidunt. Ac enim neque purus ac lorem lacus cursus.</p>
             <br />
-            <Goldtext Text="James cooper"/><span style={{color:"#BF8B1A", fontFamily:"poppins"}}>Client</span>
+            <Goldtext Text="James cooper"/><span style={{color:"#BF8B1A", fontFamily:"poppins"}}> Our Client</span>
         </div>
     </Container>
     </>
